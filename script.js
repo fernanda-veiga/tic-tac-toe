46eb4e1dc01384ceec3c3b64630fd3682a397c47
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-02-08 16:06:53 -0300

    Added a function on the game object that changes the turn of the players depending on which player played last.

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const gameboard = (() => {
     const squaresEventListener = (() => {
         squares.forEach(square => {
             square.addEventListener("click", () => {
-                square.textContent = "X";
+                square.textContent = game.whichMarkerNow();
+                game.changeTurn();
             });
         })
     })();
@@ -21,9 +22,20 @@ const player = (playerMarker, playerTurn) => {
     return {marker, turn};
 }
 
-const game = () => {
-    return {}
-};
-
 let playerOne = player("X", 1);
 let playerTwo = player("O", 0);
+
+const game = (() => {
+    const whichMarkerNow = () => {
+        return (playerOne.turn === 1) ? playerOne.marker : playerTwo.marker;
+    }
+    const changeTurn = () => {
+        if (playerOne.turn === 1) {
+            playerOne.turn = 0; playerTwo.turn = 1;
+        } else {
+            playerOne.turn = 1; playerTwo.turn = 0;
+        }
+    }
+    return {whichMarkerNow, changeTurn};
+})();
+
